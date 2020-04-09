c6766d3e9aef0214a6ee59049f5b4d5ca6149659
Peter Jung <peterjung617@gmail.com>
2020-04-09 17:21:35 -0400

    Added Navbar

---
+++ src/Components/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Peter Jung</h1>
+                <h3>Computer Science major - Purdue University</h3>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h5>Home</h5>
+                <h5>Contact</h5>
+                <h5>About</h5>
+            </div>
+        )
+    }
+}
